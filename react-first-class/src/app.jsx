49476b8340fdd69b7f49883f9f35a341bd823dc4
faima-ahmed <faimaahmeddiya@gmail.com>
2025-06-29 15:09:14 +0600

    complete-useEffect

--- react-first-class/src/app.jsx
+++ react-first-class/src/app.jsx
@@ -1,19 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [todoTitle, setTodoTitle] = useState("");
 
-  fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5`)
-    .then((res) => res.json())
-    .then((data) => {
-      setPosts(data);
+  const fetchAllTodos = async () => {
+    fetch(`http://localhost:3000/todos`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  };
+  useEffect(() => {
+    fetchAllTodos();
+  }, []);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("I am from submit handler");
+    console.log(todoTitle, "todotitle");
+
+    fetch(`http://localhost:3000/todos`, {
+      method: "POST",
+      body: JSON.stringify({ title: todoTitle, isCompleted: false }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }).then(() => {
+      fetchAllTodos();
+      setTodoTitle("");
     });
+  };
+
+  const removeHandler = (todoId) => {
+    fetch(`http://localhost:3000/todos/${todoId}`, {
+      method: "DELETE",
+    }).then(() => {
+      fetchAllTodos();
+    });
+  };
   return (
     <>
-      <h2>Post List</h2>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {/* <hr /> */}
+      <h2>Add Todos</h2>
       <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <button onClick={()=> removeHandler(todo.id)}>Remove Todo</button>
+          </li>
         ))}
       </ul>
     </>
