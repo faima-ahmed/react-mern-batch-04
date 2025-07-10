0a72275eb26242d414ed5c45b3225c6681eb6961
faima-ahmed <faimaahmeddiya@gmail.com>
2025-07-10 09:34:21 +0600

    redux-used

--- redux-sumit/src/App.jsx
+++ redux-sumit/src/App.jsx
@@ -1,18 +1,41 @@
 import Counter from "./components/Counter";
 import Stats from "./components/Stats";
 
-export default function App() {
-    return (
-        <div className="w-screen h-full p-10 bg-gray-100 text-slate-700">
-            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
-                Simple Counter Application
-            </h1>
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./features/counters/countersSlice";
 
-            <div className="max-w-md mx-auto mt-10 space-y-5">
-                <Counter />
-                <Counter />
-                <Stats totalCount={10}/>
-            </div>
-        </div>
-    );
+export default function App() {
+  const counters = useSelector((state) => state.counters);
+  const dispatch = useDispatch();
+
+  const handleIncrement = (counterId) => {
+   dispatch(increment(counterId));
+  };
+
+  const handleDecrement = (counterId) => {
+   dispatch(decrement(counterId));
+  };
+
+  //derived state
+  const totalCount = counters.reduce((sum, cur) => sum + cur.value, 0);
+
+  return (
+    <div className="w-screen h-full p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            count={counter.value}
+            onIncrement={() => handleIncrement(counter.id)}
+            onDecrement={() => handleDecrement(counter.id)}
+          />
+        ))}
+        <Stats totalCount={totalCount} />
+      </div>
+    </div>
+  );
 }
---
+++ redux-sumit/src/features/counters/countersSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import initialCounters from "../../data/initialCounters";
+
+const countersSlice = createSlice({
+  name: "counters",
+  initialState: initialCounters,
+  reducers: {
+    //we will get counterId from action.payload
+    increment: (state, action) => {
+      const counterIndex = state.findIndex((c) => c.id === action.payload);
+      state[counterIndex].value++;
+    },
+
+    decrement: (state, action) => {
+      const counterIndex = state.findIndex((c) => c.id === action.payload);
+      state[counterIndex].value--;
+    },
+  },
+});
+
+export default countersSlice.reducer;
+export const { increment, decrement } = countersSlice.actions;
